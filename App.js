f1f7033472943def01ce1739c02960c9c74d50fd
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-03-24 13:49:54 +0530

    Created a todo app with Alert

--- App.js
+++ App.js
@@ -1,57 +1,56 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import {
-  Button,
-  FlatList,
-  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   View,
   TouchableOpacity,
+  FlatList,
+  Alert,
 } from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: "shakun", id: "1" },
-    { name: "yoshie", id: "2" },
-    { name: "mario", id: "3" },
-    { name: "luigi", id: "4" },
-    { name: "peach", id: "5" },
-    { name: "toad", id: "6" },
-    { name: "bowser", id: "7" },
-    { name: "Ocean", id: "8" },
-    { name: "Perry", id: "9" },
-    { name: "Sagar", id: "10" },
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
   ]);
 
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter((person) => person.id != id);
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
     });
+  };
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OOPS!", "Tasks must be over 3 char long", [
+        { text: "Got It", onPress: () => console.log("alert Closed") },
+      ]);
+    }
   };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
-      {/* <ScrollView>
-        {people.map((item) => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -60,17 +59,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    //alignItems: "center",
-    //justifyContent: "center",
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "cyan",
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
